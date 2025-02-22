d17232f92c28fb37752ae123164e7e1d14963786
Bharat Gusaiwal <bharatgusaiwal731@gmail.com>
2025-02-22 22:12:41 +0530

    Implement data ingestion module and update requirements; enhance error handling and logging

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-
+from src.logger import logging
 def error_message_details(error, erro_detail: sys):
     _, _, exc_tb = erro_detail.exc_info()
     error_message = (
@@ -17,7 +16,14 @@ def error_message_details(error, erro_detail: sys):
     return error_message
 class CustomException(Exception):
     def __init__(self,error_message,erro_detail:sys):
-        super.__init__(error_message)
-        self.error_message = error_message_details(error_message,erro_detail)
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,erro_detail=erro_detail)
     def __str__(self):
         return self.error_message
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e,sys)
