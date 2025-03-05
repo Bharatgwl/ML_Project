c414b2352da3d0a83646ca7471970decc590b54b
Bharat Gusaiwal <bharatgusaiwal731@gmail.com>
2025-03-05 22:26:00 +0530

    prediction pipeline created

--- src/util.py
+++ src/util.py
@@ -58,3 +58,11 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     except Exception as e:
         logging.info("Exception occured in evaluate_models function")
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
